fd0f13cc0fac2016e7b65910f3f3f1221890715e
Marrowslicer <marrowslicer@gmail.com>
2020-09-13 01:30:54 +0300

    Add AnswerGenerator class

---
+++ MagicEightBall/MagicEightBall/AnswerGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MagicEightBall
+{
+    class AnswerGenerator
+    {
+        private static string[] answers = new string[]
+        {
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes – definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful."
+        };
+
+        public static string GenerateAnswer()
+        {
+            var generator = new Random();
+            var answer = answers[generator.Next(0, answers.Length)];
+
+            return answer;
+        }
+    }
+}
